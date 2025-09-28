16d490c8cbaf124678d8921ad49e17862097ccbf
Jigglypuf430 <cbush2025@waverley.nsw.edu.au>
2025-09-28 06:16:53 +0000

    mj

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,9 @@
 import DigitalLicense from "./components/DigitalLicense";
 
 export default function App() {
-  return <DigitalLicense />;
+  return (
+    <div style={{ minHeight: "100vh", background: "#0b1220", color: "#e5e7eb", display: "grid", placeItems: "center", padding: 24 }}>
+      <DigitalLicense />
+    </div>
+  );
 }
